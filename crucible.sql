1e26e1aff9ca2b6aef58008a4e68a18c2f3d8f93
Rani Sargees <cheeze.sprinkels+official_git@gmail.com>
2018-01-29 17:38:36 -0500

    fixed db again

--- crucible.sql
+++ crucible.sql
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `episodes` (
   `link` text NOT NULL,
   `season` int(11) NOT NULL,
   `episode` int(11) NOT NULL,
-  `ismovie` int(11) NOT NULL DEFAULT '0',
+  `ismovie` tinyint(1) NOT NULL DEFAULT '0',
   `id` int(11) NOT NULL AUTO_INCREMENT,
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=23 ;
